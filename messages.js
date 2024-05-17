ead2c13aa7ae618ab115a76e852f658aa3718d32
Michilis <120072772+Michilis@users.noreply.github.com>
2024-05-17 23:10:03 +0200

    Update messages.js

--- messages.js
+++ messages.js
@@ -3,5 +3,13 @@ module.exports = {
   topWalletsMessage: (wallets) => `Top Wallets:\n${JSON.stringify(wallets, null, 2)}`,
   mintRequestMessage: (pr, hash) => `Payment request: ${pr}\nHash: ${hash}`,
   tokenMessage: (encoded) => `Encoded Token: ${encoded}`,
-  errorMessage: 'Sorry, there was an error processing your request.'
+  errorMessage: 'Sorry, there was an error processing your request.',
+  requestHashMessage: 'Please provide the hash from the mint request.',
+  helpMessage: 'Available commands:\n' +
+               '/Cashu_topwallets - Show top wallets\n' +
+               '/Cashu_topmints - Show top mints\n' +
+               '/cashu_decode - Decode a token\n' +
+               '/cashu_encode - Encode a token\n' +
+               '/help - Show this help message',
+  startMessage: 'Welcome to the CashuMints bot! Use /help to see available commands.'
 };
