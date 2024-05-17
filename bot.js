0850ff754923c8835ba990bf4e88a4b89d603053
Michilis <120072772+Michilis@users.noreply.github.com>
2024-05-17 23:10:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,10 +4,20 @@ const commands = require('./Modules/commands');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.command('/cashu_mints', commands.cashuMints);
-bot.command('/cashu_wallets', commands.cashuWallets);
+bot.start(commands.start);
+bot.help(commands.help);
+bot.command('/Cashu_topwallets', commands.cashuTopWallets);
+bot.command('/Cashu_topmints', commands.cashuTopMints);
+bot.command('/cashu_decode', commands.decodeToken);
+bot.command('/cashu_encode', commands.encodeToken);
 bot.command('/request_mint', commands.requestMint);
 bot.command('/check_invoice', commands.checkInvoice);
+
+bot.action(/^show_qr_(.+)$/, async (ctx) => {
+  const url = ctx.match[1];
+  const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(url)}&size=150x150`;
+  ctx.replyWithPhoto(qrCodeUrl, { caption: `Mint URL: ${url}` });
+});
 
 bot.launch();
 
