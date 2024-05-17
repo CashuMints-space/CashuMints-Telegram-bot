106800a77fe31cc8af8a30a8dfe4d3f7c129f6d5
Michilis <120072772+Michilis@users.noreply.github.com>
2024-05-17 23:36:50 +0200

    Update messages.js

--- messages.js
+++ messages.js
@@ -1,17 +1,41 @@
 module.exports = {
-  topMintsMessage: (mint) => `*Mint:* ${mint.name}\n*URL:* ${mint.url}`,
-  topWalletsMessage: (wallet) => `*Wallet:* ${wallet.name}\n*URL:* ${wallet.url}`,
-  mintRequestMessage: (pr, hash) => `Payment request: ${pr}\nHash: ${hash}`,
-  tokenMessage: (encoded) => `Encoded Token: ${encoded}`,
-  errorMessage: 'Sorry, there was an error processing your request.',
-  requestHashMessage: 'Please provide the hash from the mint request.',
-  helpMessage: 'Available commands:\n' +
-               '/cashu topwallets - Show top wallets\n' +
-               '/cashu topmints - Show top mints\n' +
-               '/cashudecode - Decode a token\n' +
-               '/cashuencode - Encode a token\n' +
-               '/request mint - Request minting tokens\n' +
-               '/check invoice - Check invoice status and get tokens\n' +
-               '/help - Show this help message',
-  startMessage: 'Welcome to the CashuMints bot! Use /help to see available commands.'
+  pendingMessage: (username, token, cashuApiUrl) => `
+${username} shared a Cashu token 🥜
+
+Click here to claim to Lightning: [Claim link](${cashuApiUrl}?token=${token})
+  `,
+  claimedMessage: (username) => `
+${username} shared a Cashu token 🥜
+
+Cashu token has been claimed ✅
+  `,
+  errorMessage: 'Error processing your request. Please try again later.',
+  checkingTokenStatus: 'Checking token status...',
+  tokenStatusButtonPending: 'Token Status: Pending',
+  tokenStatusButtonClaimed: 'Token Status: Claimed',
+  helpMessage: `
+Welcome to the Cashu Claim Bot! Here’s how to get started:
+
+1. **Using the Bot in Private Chat:**
+   - Send me a Cashu token, and I’ll provide you with a QR code and the status of the token.
+
+2. **Using the Bot in Group Chats:**
+   - Add me to a group and give me admin permissions with only the 'Remove Messages' permission enabled.
+   - I only need this permission to remove Cashu tokens after processing them to keep the chat clean and tidy.
+
+Happy Satoshi hunting!
+  `,
+  startMessage: `
+Welcome to the Cashu Claim Bot! Here’s how to get started:
+
+1. **Using the Bot in Private Chat:**
+   - Send me a Cashu token, and I’ll provide you with a QR code and the status of the token.
+
+2. **Using the Bot in Group Chats:**
+   - Add me to a group and give me admin permissions with only the 'Remove Messages' permission enabled.
+   - I only need this permission to remove Cashu tokens after processing them to keep the chat clean and tidy.
+
+Happy Satoshi hunting!
+  `,
 };
+
