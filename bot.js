578dd70cb8afd03d1912128664a7b73df85a7317
Michilis <120072772+Michilis@users.noreply.github.com>
2024-05-17 23:36:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const { Telegraf } = require('telegraf');
 const commands = require('./Modules/commands');
+const { handleMessage } = require('./Modules/cashuchecker');
+const messages = require('./messages');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const cashuApiUrl = process.env.CASHU_API_URL;
+const claimedDisposeTiming = parseInt(process.env.CLAIMED_DISPOSE_TIMING) || 10;
 
 bot.start(commands.start);
 bot.help(commands.help);
@@ -18,6 +22,12 @@ bot.action(/^show_qr_(.+)$/, async (ctx) => {
   const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(url)}&size=150x150`;
   console.log(`[INFO] Generating QR code for URL: ${url}`);
   ctx.replyWithPhoto(qrCodeUrl, { caption: `Mint URL: ${url}` });
+});
+
+bot.on('message', async (msg) => {
+  if (msg.text && msg.text.startsWith('cashu')) {
+    await handleMessage(bot, msg, cashuApiUrl, claimedDisposeTiming);
+  }
 });
 
 bot.launch();
