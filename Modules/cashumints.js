b8ed0a9cbd40a05a9105d4d7a1ff8538c001e67a
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-16 16:57:20 +0200

    Update cashumints.js

--- Modules/cashumints.js
+++ Modules/cashumints.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const { saveData, loadData } = require('./dataCache');
+const logger = require('../logger');
 
 const fetchAndCacheData = async (url, cacheFilename) => {
     const cachedData = loadData(cacheFilename);
@@ -12,7 +13,7 @@ const fetchAndCacheData = async (url, cacheFilename) => {
         saveData(cacheFilename, response.data);
         return response.data;
     } catch (error) {
-        console.error(`Error fetching data from ${url}:`, error);
+        logger.error(`Error fetching data from ${url}:`, error);
         return null;
     }
 };
@@ -21,8 +22,4 @@ const getTopMints = async () => {
     return await fetchAndCacheData('https://cashumints.space/wp-json/public/top-liked-public/', 'top_mints.json');
 };
 
-const getTopWallets = async () => {
-    return await fetchAndCacheData('https://cashumints.space/wp-json/public/top-liked-public/', 'top_wallets.json');
-};
-
-module.exports = { getTopMints, getTopWallets };
+module.exports = { getTopMints };
